cb5e390401dc8ccf2497bc5ee71ffd61ae7666d9
Ivan Trujillo <krival@Zohar.home>
2016-06-12 17:38:58 -0400

    Added board elements

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,11 +1,24 @@
 console.log("JS file is connected to HTML! Woo!")
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
+//var cardOne = 'queen';
+//var cardTwo = 'queen';
+//var cardThree = 'king';
+//var cardFour = 'king';
 
-if (cardOne === cardTwo) {
-	alert('You found a match!');	
-} else {
-  alert('Sorry, try again.');
-}
+//*if (cardOne === cardTwo) {
+//*	alert('You found a match!');	
+//*} else {
+//*  alert('Sorry, try again.');
+//*}
+
+var gameBoard = document.getElementByID('game-board');
+//variable of my board
+
+function createBoard() {
+ for (var i = 0; i < cards.length; i++) {
+ 		var memoryCard = document.createElement('div')
+ 		cardElement.className = 'card';
+
+ 		board.appendChild(cardElement);
+ 	}
+
+ }
